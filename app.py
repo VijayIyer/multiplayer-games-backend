aefda9c70ef181b9cfe1bcb508fd193b09a76949
vijay <VijayIyer>
2023-04-29 18:58:46 -0400

    renamed app to application

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from app import create_app, socket
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    print(f'running socketio server')
+    print('running socketio server')
     socket.run(app, host='0.0.0.0')
